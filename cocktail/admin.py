74ecec1fb51fde61857eea76f6c9366e051a77fb
이경호 <rud0503@gmail.com>
2022-09-13 17:28:16 +0900

    Add UserAdmin

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
 from .models import *
 from account.models import *
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from account.forms import UserChangeForm, UserCreationForm
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'age', 'username', 'date_of_birth', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'age', 'username')}),
+        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'age', 'username', 'date_of_birth', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
 
 admin.site.register(Cocktail)
 admin.site.register(Ingredient)
 admin.site.register(Post)
 admin.site.register(Category)
-admin.site.register(User)
