81da4bcf535ac783826c36f88a263548eb7a2726
이경호 <rud0503@gmail.com>
2022-09-13 17:30:35 +0900

    Add UserCreateAPIView

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.serializers import UserCreateSerializer
+from account.renderers import UserJSONRenderer
+
+class UserCreateAPIView(APIView):
+    permission_classes = (AllowAny,)    
+    #permission_classes = 누가 이 view를 사용 가능한지에 대한 범위를 결정
+    # 사용자 등록은 누가나 가능해야 하기때문에 AllowAny로 설정
+    serializer_class = UserCreateSerializer
+    
+    def post(self, request):
+        user = request.data     #요청한 데이터를 받아오기        
+        serializer = self.serializer_class(data=user)       #직렬화
+        serializer.is_valid(raise_exception=True)   #유효성 확인
+        serializer.save()   #저장
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #직렬화된 데이털르 반환
