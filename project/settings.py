9ff3d5783f3e57596010edbd15cc150f9da47539
이경호 <rud0503@gmail.com>
2022-09-13 17:27:00 +0900

    add account

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     
     'cocktail',
     'rest_framework',
-    'account'
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
