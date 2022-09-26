a1bb137dec4a89e1d323f743009740cee5a07f8b
이경호 <rud0503@gmail.com>
2022-09-26 16:39:57 +0900

    Add user-list, user-update url

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from account.views import UserCreateAPIView, LoginAPIView
+from account.views import UserCreateAPIView, LoginAPIView, UserListAPIView, UserRetrieveUpdateView
 urlpatterns = [
     path('create/', UserCreateAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
+    path('current', UserRetrieveUpdateView.as_view()),
+    path('user/', UserListAPIView.as_view()),
 ]
