3abda49c72e02f20108ae6a5d9d3a6e74fe28651
이경호 <rud0503@gmail.com>
2022-09-26 16:38:56 +0900

    Add UserUpdateSerializer and UserListSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -51,3 +51,38 @@ class LoginSerializer(serializers.Serializer):
         user.save(update_fields = ['last_login'])
         
         return {'email' : user.email, 'username' : user.username, 'last_login' : user.last_login}
+    
+class UserUpdateSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField (
+        max_length = 128,
+        min_length = 4,
+        write_only = True
+    )
+    
+    class Meta:
+        model = User
+        fields = ['email', 'age', 'username', 'date_of_birth', 'password', 'token']
+        
+        read_only_fields = ('toekn',)   #read_only_fields = read_only = True와 같은 역할
+        #token 필드는 속성값 지정이 없기 때문에 read_only_fields 사용
+        
+    def update(self,instance, validated_data):      #사용자의 정보를 업데이트 할 때 실행
+        password = validated_data.pop('password', None)     #pop 함수를 이용해 password 제거
+        
+        for(key, value) in validated_data.items():
+            setattr(instance, key, value)   #setattr(객체, 속성명, 속성값) - 정의된 속성값을  바꾸거나 새롭게 속성을 추가할 때 사용
+            # instance 안에 있는 key의 값을 바꿔줌 - User의 속성값을 변경
+            
+        if password is not None:
+            instance.set_password(password)
+            
+        instance.save()     #instance에 저장 DB에 저장되지는 않음 DB 저장을 view에서 진행
+        
+        return instance
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'age', 'date_of_birth', 'token']
+        
