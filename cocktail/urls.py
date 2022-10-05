608b05188ef2fcecfcd367db89e711cdd8435854
이경호 <rud0503@gmail.com>
2022-10-05 17:13:29 +0900

    add cocktail CR urls

--- cocktail/urls.py
+++ cocktail/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
+from cocktail.views import CocktailCreateAPIView, CocktailListAPIView
 urlpatterns = [
     path('account/', include('account.urls')),
+    path('cocktail/', CocktailListAPIView.as_view()),
+    path('create-cocktail/', CocktailCreateAPIView.as_view()),
 ]
