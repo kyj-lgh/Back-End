db1ba88c8f123c2bc385420cf6dc365eeae3f128
이경호 <rud0503@gmail.com>
2022-10-05 17:14:00 +0900

    add cocktail CR APIView

--- cocktail/views.py
+++ cocktail/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from rest_framework.views import APIView
 
-# Create your views here.
+from cocktail.models import Cocktail
+from cocktail.serializers import CocktailListSerializer, CocktailCreateSerializer
+
+class CocktailListAPIView(ListAPIView):
+    permission_classes=(AllowAny,)
+    queryset = Cocktail.objects.all()
+    serializer_class = CocktailListSerializer
+    
+class CocktailCreateAPIView(APIView):
+    permission_classes=(AllowAny,)
+    
+    def post(self, request):
+        serializer = CocktailCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
