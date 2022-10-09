ebb0454570ae79cce37193733ba4a68aaa21c5a2
이경호 <rud0503@gmail.com>
2022-10-09 15:53:49 +0900

    combine list/create serializer

--- cocktail/serializers.py
+++ cocktail/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from cocktail.models import Cocktail, Category, Ingredient
 
-class CocktailListSerializer(serializers.ModelSerializer):
-    category = serializers.CharField(source='category.name')
-    ingredient = serializers.SlugRelatedField(slug_field='name', many=True, read_only=True)
+class CocktailSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', queryset = Category.objects.all())
+    ingredient = serializers.SlugRelatedField(slug_field='name', many=True, queryset = Ingredient.objects.all())
     class Meta:
         model = Cocktail 
         fields = ['id', 'category', 'name', 'alcohol', 'ingredient', 'image', 'create_dt', 'bookmark']
@@ -20,8 +20,10 @@ class IngerdientSerializer(serializers.ModelSerializer):
         fields = ['name']
         
         
-class CocktailCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cocktail
-        fileds='__all__'
-        exclude= ['update_dt']
+# class CocktailCreateSerializer(serializers.ModelSerializer):
+#     category = serializers.SlugRelatedField(slug_field='name', queryset = Category.objects.all())
+#     ingredient = serializers.SlugRelatedField(slug_field='name', many=True, queryset = Ingredient.objects.all())
+#     class Meta:
+#         model = Cocktail
+#         fileds='__all__'
+#         exclude= ['update_dt']
