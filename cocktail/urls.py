fa70fdcc8e8abba25c97b0b23fdb777bdd8e53fc
이경호 <rud0503@gmail.com>
2022-10-09 15:54:04 +0900

    update url

--- cocktail/urls.py
+++ cocktail/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
-from cocktail.views import CocktailCreateAPIView, CocktailListAPIView
+from cocktail.views import CocktailAPIView, CocktaildetailAPIView
 urlpatterns = [
     path('account/', include('account.urls')),
-    path('cocktail/', CocktailListAPIView.as_view()),
-    path('create-cocktail/', CocktailCreateAPIView.as_view()),
+    path('', CocktailAPIView.as_view()),
+    path('<int:pk>/', CocktaildetailAPIView.as_view()),
 ]
