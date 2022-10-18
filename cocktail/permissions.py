eebb1a3ce54a5ff97a5d182665d7cea8ebb02891
이경호 <rud0503@gmail.com>
2022-10-18 23:31:26 +0900

    permission setting

--- cocktail/permissions.py
+++ cocktail/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 class CustomReadOnly(permissions.BasePermission):
-    #글 조회 : 누구나, 글 생성 : 로그인한 유저, 편집 : 글 작성자
+    #메뉴 조회 : 누구나, 메뉴 생성,수정,삭제 : 관리자만
     def has_permission(self, request, view):
         if request.method == "GET":
             return True
