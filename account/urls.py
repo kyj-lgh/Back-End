64c628bc4c37a32894191e75047291f46799b9e9
이경호 <rud0503@gmail.com>
2022-10-20 23:50:44 +0900

    경로 설정

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from account.views import UserCreateAPIView, LoginAPIView, UserListAPIView, UserRetrieveUpdateView
+from account.views import UserCreateAPIView, LoginAPIView, UserListAPIView, UserDetailAPIView
 urlpatterns = [
     path('user/create/', UserCreateAPIView.as_view()),
     path('user/login/', LoginAPIView.as_view()),
-    path('user/edit/', UserRetrieveUpdateView.as_view()),
+    path('user/<int:pk>/', UserDetailAPIView.as_view()),
     path('user/list/', UserListAPIView.as_view()),
 ]
