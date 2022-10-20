420d5162196ea7f1f95ff31038072fe2b41a8190
이경호 <rud0503@gmail.com>
2022-10-20 23:48:41 +0900

    권한 설정

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
