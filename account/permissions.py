f8cc4c5f2059b2f6d0b56ac074b8d14242e9f0fe
이경호 <rud0503@gmail.com>
2022-10-21 10:38:16 +0900

    custom 권한 설정

--- account/permissions.py
+++ account/permissions.py
@@ -5,9 +5,9 @@ class CustomReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "GET":
             return True
-        return request.user.is_authenticated and request.user.is_admin
+        return request.user.is_authenticated
     
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_admin
+        return request.user.is_admin or obj.pk == request.user.pk
